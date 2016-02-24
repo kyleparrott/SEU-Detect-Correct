581d7358a8cb3a79a8d40f1e1f7e787cfdffaf4d
kyleparrott <kyle.parrott@gmail.com>
2016-02-24 16:10:45 -0600

    updated to use the flash peripheral interface

--- seu/src/trace_functions.c
+++ seu/src/trace_functions.c
@@ -16,8 +16,12 @@
  */
 
 #include <stm32f4xx.h>
+#include <stm32f4xx_flash.h>
 #include "crc.h"
+
 const uint32_t BLK_15_START = 0x080C0000;
+const uint8_t WORK_FLASH_SECTOR = 10; //Just using sector 10 as the temporary work area, Sector11 still hold program data.
+
 
 block_header_t __attribute__((section(".flash00hdr"))) FlashHeader0;
 block_header_t __attribute__((section(".flash01hdr"))) FlashHeader1;
@@ -201,27 +205,51 @@ void __cyg_profile_func_exit(void *func, void *caller) {
 }
 
 void flash_copy_to_work(uint32_t* blk_ptr, uint32_t blk_size){
+	FLASH_Unlock();
+	FLASH_SetLatency(FLASH_Latency_5); //My discovery board running @ 168 mhz, 2.907v
+	FLASH_EraseSector(WORK_FLASH_SECTOR, VoltageRange_3);
 	uint32_t idx;
-	uint32_t* dest = (uint32_t*) BLK_15_START;
+	uint32_t dest = BLK_15_START;
 	for (idx = 0; idx < blk_size; idx++) {
-		dest[idx] = blk_ptr[idx];
+		FLASH_ProgramWord(dest + idx, *blk_ptr + idx);
 	}
+	FLASH_Lock();
 }
 
  void flash_copy_from_work(uint32_t* blk_ptr, uint32_t blk_size, uint32_t* fix_ptrs[16], uint32_t fix_values[16], uint32_t fix_count) {
  	uint32_t* src_blk = (uint32_t*) BLK_15_START;
  	uint32_t* new_address = 0;
- 	uint32_t  idx;
  	uint8_t  fixup_index = 0; // keeps track of fix_ptrs, fix_values iterations
 
+ 	FLASH_Unlock();
+	FLASH_SetLatency(FLASH_Latency_5); //My discovery board running @ 168 mhz, 2.907v
+	FLASH_EraseSector(WORK_FLASH_SECTOR, VoltageRange_3);
+
+ 	uint32_t  idx;
  	for (idx = 0; idx < blk_size; idx++) {
  		new_address = blk_ptr + idx; 
 
  		if (new_address == fix_ptrs[fixup_index]) { // this assumes the fix_ptrs[] elements are sorted
- 			blk_ptr[idx] = fix_values[fixup_index];
+ 			FLASH_ProgramWord((uint32_t) new_address, fix_values[fixup_index]);
  			fixup_index++;
  		} else {
- 			blk_ptr[idx] = src_blk[idx];	
+ 			FLASH_ProgramWord((uint32_t) new_address, src_blk[idx]);
  		}
  	}
+    FLASH_Lock();
  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
