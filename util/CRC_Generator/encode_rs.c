593cae8ab227009a9fdc076c3936802c165534d8
kyleparrott <kyle.parrott@gmail.com>
2016-02-26 15:25:26 -0600

    updated working directory state

--- util/CRC_Generator/encode_rs.c
+++ util/CRC_Generator/encode_rs.c
@@ -4,36 +4,36 @@
  */
 #include "rs.h"
 
-void encode_rs(symbol_t* data)
+void encode_rs(symbol_t* data, symbol_t* parity)
 {
 	int i, j;
 	symbol_t feedback;
-	symbol_t parity[PARITY_SYMBOL_COUNT];
+	symbol_t parity_words[PARITY_SYMBOL_COUNT];
 
 	for(i=0; i<PARITY_SYMBOL_COUNT; i++) {
-		parity[i] = 0;
+		parity_words[i] = 0;
 	}
 
 	for (i = 0; i < DATA_SYMBOL_COUNT; i++) {
-		feedback = symbol_get(index_of, symbol_get(data, i) ^ parity[0]);
+		feedback = symbol_get(index_of, symbol_get(data, i) ^ parity_words[0]);
 		if (feedback != SYMBOL_MASK) { /* feedback term is non-zero */
 			for (j = 1; j < PARITY_SYMBOL_COUNT; j++) {
-				parity[j] ^= symbol_get(alpha_to, modnn(feedback + symbol_get(genpoly, PARITY_SYMBOL_COUNT - j)));
+				parity_words[j] ^= symbol_get(alpha_to, modnn(feedback + symbol_get(genpoly, PARITY_SYMBOL_COUNT - j)));
 			}
 		}
 		/* Shift */
 		for(j=0; j < PARITY_SYMBOL_COUNT - 1; j++) {
-			parity[j] = parity[j+1];
+			parity_words[j] = parity_words[j+1];
 		}
 		if (feedback != SYMBOL_MASK) {
-			parity[PARITY_SYMBOL_COUNT - 1] = symbol_get(alpha_to, modnn(feedback + symbol_get(genpoly, 0)));
+			parity_words[PARITY_SYMBOL_COUNT - 1] = symbol_get(alpha_to, modnn(feedback + symbol_get(genpoly, 0)));
 		}
 		else {
-			parity[PARITY_SYMBOL_COUNT - 1] = 0;
+			parity_words[PARITY_SYMBOL_COUNT - 1] = 0;
 		}
 	}
 
 	for(i=0; i<PARITY_SYMBOL_COUNT; i++) {
-		symbol_put(data, PARITY_START + i, parity[i]);
+		symbol_put(parity, i, parity_words[i]);
 	}
 }
